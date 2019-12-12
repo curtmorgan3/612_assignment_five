dc6438ad1f394770ae73833db416e30013f13875
Curt Morgan <curtmorgan3@gmail.com>
2019-12-11 19:55:53 -0500

    docker

--- models.js
+++ models.js
@@ -1,13 +1,25 @@
 const Sequelize = require('sequelize');
 const DATABASE_NAME = "marvel_db";
 
-const sequelize = new Sequelize({
-	database: DATABASE_NAME,
+// const sequelize = new Sequelize({
+// 	database: DATABASE_NAME,
+// 	dialect: 'postgres',
+// 	operatorsAliases: false,
+// 	define: {
+// 		underscored: true
+// 	},
+// });
+console.log(process.env.DATABASE_URL);
+const sequelize = new Sequelize('postgres://postgres:password@localhost:5432/marvel_db', {
 	dialect: 'postgres',
+	protocol: 'postgres',
 	operatorsAliases: false,
 	define: {
 		underscored: true
 	},
+	dialectOptions: {
+		ssl: true
+	}
 });
 
 const Character = sequelize.define('character', {
